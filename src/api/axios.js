5a90ea37152809d827b15d53a0856af455c7047c
girichandu12321513 <150949033+girichandu12321513@users.noreply.github.com>
2025-12-21 23:07:32 +0530

    Use Vercel env variable for API base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "https://fleet-backend-w0b6.onrender.com/api",
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
 
-export default api;
+export default instance;
